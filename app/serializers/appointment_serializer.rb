46a4d88195508593fb1f5afaefbd52610dd64e93
rails <rahul9713428971@gmail.com>
2024-03-22 17:49:01 +0530

    minor changes

--- app/serializers/appointment_serializer.rb
+++ app/serializers/appointment_serializer.rb
@@ -2,7 +2,15 @@ class AppointmentSerializer
   include FastJsonapi::ObjectSerializer
 
   set_key_transform :underscore
-  attributes :id, :name, :email, :phone, :date
+  attributes :id, :name, :email, :phone
+
+  attribute :status do |object| 
+    object.status.capitalize()
+  end
+
+  attribute :date do |object| 
+    object.date.strftime("%A, %B %d, %Y")
+  end
 
   attribute :property do |object| 
     Property.find(object.property_id)
