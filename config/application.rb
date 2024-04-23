4aaed104a244f3d18752a3190385ddc6ab6c4570
rails <rahul9713428971@gmail.com>
2024-04-23 14:25:17 +0530

    modified env configuration

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,9 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-# Dotenv::Railtie.load
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Railtie.load
+end
 
 EMAIL = ENV['EMAIL']
 PASSKEY = ENV['PASSKEY']
@@ -18,12 +20,12 @@ module RentalPropertyApi
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
-    config.before_configuration do
-      env_file = File.join(Rails.root, 'config', 'local_env.yml')
-      YAML.load(File.open(env_file)).each do |key, value|
-        ENV[key.to_s] = value
-      end 
-    end
+    # config.before_configuration do
+    #   env_file = File.join(Rails.root, 'config', 'local_env.yml')
+    #   YAML.load(File.open(env_file)).each do |key, value|
+    #     ENV[key.to_s] = value
+    #   end 
+    # end
     
     # Configuration for the application, engines, and railties goes here.
     #
